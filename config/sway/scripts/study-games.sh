0e89d92848fa25fea37944951d9396866cfe9a90
Justin <justincaveda11031@gmail.com>
2022-10-24 02:15:46 -0500

    auto-update

--- config/sway/scripts/study-games.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-swaymsg 'exec /home/petergriffin/.config/Anki/anki.sh'
-
-swaymsg 'workspace $ws4; exec nemo'
-
-swaymsg 'workspace $ws4; exec gtk-launch game2text'
-
-swaymsg 'workspace $ws5; exec steam'
