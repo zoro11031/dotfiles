3d6c73140c2d2519858c152019cd08e5fd19cc1f
zoro11031 <justincaveda11031@gmail.com>
2022-11-16 12:00:06 -0600

    auto-update

--- home/petergriffin/config/sway/scripts/start.sh
+++ home/petergriffin/config/sway/scripts/start.sh
@@ -6,7 +6,7 @@ swaymsg 'workspace $ws3; exec thunar'
 
 swaymsg 'exec flatpak run com.discordapp.DiscordCanary && move container to workspace $ws2'
 
-swaymsg 'exec spotify && move container to workspace $ws2'
+swaymsg 'exec spotify'
 
 swaymsg 'exec flatpak run com.valvesoftware.Steam --silent && move container to workspace $ws5'
 
