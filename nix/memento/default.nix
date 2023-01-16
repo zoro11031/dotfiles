30d53e9407a3a8dd49f4a9cd045c985408f8b511
zoro11031 <justincaveda11031@gmail.com>
2023-01-16 12:27:20 -0600

    auto-update

---
+++ nix/memento/default.nix
@@ -0,0 +1,55 @@
+{ lib
+, mpv
+, stdenv
+, mkDerivation
+, qtbase
+, qttools
+, qtx11extras
+, qmake
+, sqlite
+, fetchFromGitHub
+, json-c
+, libzip
+, mecab
+, mecab-ipadic
+, meson
+, mpv
+, ninja
+, nix-update-script
+, pkg-config
+, python3
+, wrapGAppsHook4
+}:
+
+stdenv.mkDerivation rec {
+  pname = "memento";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "ripose-jp";
+    repo = "Memento";
+    rev = "v${version}";
+    sha256 = "7cc9a338899ed4f0a87fb43c1dd25183220570225801f2d1e50083a91359f74c";
+  };
+
+  nativeBuildInputs = [
+    wrapQtAppsHook
+    desktop-file-utils
+    meson
+    ninja
+    pkg-config
+    python3
+    wrapGAppsHook4
+  ];
+
+  buildInputs = [
+    qtbase
+    qtx11extras
+    mpv
+  ];
+
+  doCheck = true;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = with lib;
