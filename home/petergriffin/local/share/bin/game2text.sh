3d8aa6d1c133fbfbf2ac007df28ece346ff13de5
Justin Caveda <petergriffin@nyquil-babies.duckdns.org>
2023-01-19 09:59:39 -0600

    auto-update

--- home/petergriffin/local/share/bin/game2text.sh
+++ home/petergriffin/local/share/bin/game2text.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/petergriffin/.local/share/game2text/
-env --u WAYLAND_DISPLAY QT_QPA_PLATFORM=xcb  python /home/petergriffin/.local/share/game2text/game2text.py &
+QT_QPA_PLATFORM=xcb python /home/petergriffin/.local/share/game2text/game2text.py &
