5df9cacd47f61040289d7bb8ff432781934e6ef2
Justin <justincaveda11031@gmail.com>
2025-11-18 18:23:07 -0600

    updated config

--- .p10k.zsh
+++ .p10k.zsh
@@ -1858,3 +1858,4 @@ typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
 (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
 'builtin' 'unset' 'p10k_config_opts'
+typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
